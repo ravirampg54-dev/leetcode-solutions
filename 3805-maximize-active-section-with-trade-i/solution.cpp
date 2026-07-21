9f3aa6b73513da3c78ba98151ee2692e97a8985d
ravirampg54-dev <ravirampg54@gmail.com>
2026-07-21 16:17:13 +0000

    Sync LeetCode submission Runtime - 179 ms (7.99%), Memory - 176.1 MB (9.63%)

---
+++ 3805-maximize-active-section-with-trade-i/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        int n = s.size();
+
+        int ones = 0;
+        for (char c : s)
+            if (c == '1') ones++;
+
+        string t = "1" + s + "1";
+        int m = t.size();
+
+        vector<pair<char,int>> blocks;
+
+        for (int i = 0; i < m; ) {
+            int j = i;
+            while (j < m && t[j] == t[i]) j++;
+            blocks.push_back({t[i], j - i});
+            i = j;
+        }
+
+        int ans = ones;
+
+        for (int i = 1; i + 1 < (int)blocks.size(); i++) {
+            if (blocks[i].first == '1' &&
+                blocks[i - 1].first == '0' &&
+                blocks[i + 1].first == '0') {
+
+                ans = max(ans,
+                          ones + blocks[i - 1].second + blocks[i + 1].second);
+            }
+        }
+
+        return ans;
+    }
+};
